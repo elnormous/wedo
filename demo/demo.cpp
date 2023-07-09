629962c0d6a25b2f5bfccedae46d88379c0c18a8
elnormous <elviss@elviss.lv>
2023-07-09 10:16:15 +0300

    Add Visual Studio project

---
+++ demo/demo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "wedopp.hpp"
+
+int main()
+{
+    std::cout << "LEGO WeDo\n";
+}
---
+++ include/wedopp.hpp
@@ -0,0 +1,9 @@
+#ifndef WEDOPP_HPP
+#define WEDOPP_HPP
+
+namespace wedopp
+{
+    
+}
+
+#endif
